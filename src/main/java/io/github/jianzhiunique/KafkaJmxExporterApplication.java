b8213db686f5f3b842c3acd914b80c6c3a2e78ab
renjianzhi <renjianzhi@100tal.com>
2020-08-10 11:00:23 +0800

    Initial commit

---
+++ src/main/java/io/github/jianzhiunique/KafkaJmxExporterApplication.java
@@ -0,0 +1,33 @@
+package io.github.jianzhiunique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJmxExporterApplication {
+
+	public static void main(String[] args) {
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true){
+					try {
+						Thread.sleep(Integer.MAX_VALUE);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+
+		thread.setDaemon(true);
+		thread.start();
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		SpringApplication.run(KafkaJmxExporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jianzhiunique/KafkaJmxExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jianzhiunique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJmxExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
